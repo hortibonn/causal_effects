439d75e79dbe62b123a13d3b73cab67db743ff45
CWWhitney <whitney.cory@gmail.com>
2019-07-09 10:16:03 +0200

    working arrows

--- causal_effect_test.R
+++ causal_effect_test.R
@@ -1,5 +1,17 @@
+#Using Jude Pearl's do-Calculus in R
+#https://cran.r-project.org/web/packages/causaleffect/vignettes/causaleffect.pdf
+#https://cran.r-project.org/web/packages/causaleffect/causaleffect.pdf
+
+#using do-calculus ####
 library(causaleffect)
 library(igraph)
+
+g <- graph.formula(Temp -+ chill)
+plot(g)
+
+g <- graph.formula(Temp +- chill) #reverse arow
+plot(g)
+
 
 g <- graph.formula(Temp -+ test, test -+ Temp, team -+ Temp, test -+ team)
 plot(g)
@@ -11,3 +23,8 @@ plot(g)
 f <- graph.formula(W -+ Z, Z -+ X, X -+ Y, W -+ Y, # Observed edges
                    W -+ Y, Y -+ W, Z -+ Y, Y -+ Z, Z -+ X, X -+ Z, simplify = FALSE)
 plot(f)
+
+#using igraph####
+#https://kateto.net/netscix2016.html
+
+
