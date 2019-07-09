815a72f0977be0a815100ee52cb6a3e89b11cc83
CWWhitney <whitney.cory@gmail.com>
2019-07-09 09:44:53 +0200

    @katjaschiffers examples for our discussion today

---
+++ bnlearn_test.R
@@ -0,0 +1,35 @@
+#test of the hill climbing 'greedy' bn with bnlearn
+#Tsamardinos, Ioannis, Laura E. Brown, and Constantin F. Aliferis. “The max-min hill-climbing Bayesian network structure learning algorithm.” Machine learning 65.1 (2006): 31-78.
+
+library(bnlearn)
+data(coronary)
+
+#create the network ####
+bn_df <- data.frame(coronary)
+#(structure finding creates conditional dependency)
+#quick and dirty
+#use hc options (i.e. start) for describing DAC
+res <- hc(bn_df) #max-min hill climbing from 
+plot(res)
+
+#remove the link between M.Work and Family
+
+res$arcs <- res$arcs[-which((res$arcs[,'from'] == 
+            "M..Work" & res$arcs[,'to'] == "Family")),]
+plot(res)
+
+#bn.fit runs the ensemble EM algorithm to learn CPT
+fittedbn <- bn.fit(res, data = bn_df)
+
+print(fittedbn$Proteins)
+
+#inferring from the network####
+#chance that a non-smoker with pressure >140 has Protein level <3?
+
+cpquery(fittedbn, event = (Proteins=="<3"), 
+        evidence = ( Smoking=="no" & Pressure==">140" ) )
+
+#Protein < 3, chance of Pressure > 140?
+cpquery(fittedbn, event = (Pressure==">140"), 
+        evidence = ( Proteins=="<3" ) )
+
---
+++ causal_effect_test.R
@@ -0,0 +1,13 @@
+library(causaleffect)
+library(igraph)
+
+g <- graph.formula(Temp -+ test, test -+ Temp, team -+ Temp, test -+ team)
+plot(g)
+
+g <- set.edge.attribute(graph = g, name = "description", index = 1:2, value = "U")
+plot(g)
+
+# simplify = FALSE to allow multiple edges
+f <- graph.formula(W -+ Z, Z -+ X, X -+ Y, W -+ Y, # Observed edges
+                   W -+ Y, Y -+ W, Z -+ Y, Y -+ Z, Z -+ X, X -+ Z, simplify = FALSE)
+plot(f)
